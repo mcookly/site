e67ec2dad1249563c6fa669ba879a76011a1416d
Maximilian Cook <mcookly@fclr.net>
2025-11-15 18:15:17 -0500

    add initial list of scores

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,1 +1,18 @@
-export default async function (eleventyConfig) {}
+import { parse } from "csv-parse/sync";
+
+export default async function (eleventyConfig) {
+    eleventyConfig.setMarkdownTemplateEngine = "njk";
+
+    // Files for the user are available in `public`.
+    eleventyConfig.addPassthroughCopy("public");
+
+    // Music scores are stored as a csv, which 11ty does not recognize
+    // by default.
+    eleventyConfig.addDataExtension("csv", (contents) => {
+        const scores = parse(contents, {
+            columns: true,
+            skip_empty_lines: true,
+        });
+        return scores;
+    });
+}
